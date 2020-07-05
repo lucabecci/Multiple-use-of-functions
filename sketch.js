9b38d9fb5fa6184ab3dda01104dac365d77f0e34
lukaBRI <beccibrian@gmail.com>
2020-07-05 20:46:20 -0300

    first commit

---
+++ sketch.js
@@ -0,0 +1,19 @@
+function setup() {
+  createCanvas(600, 400);
+}
+
+function draw() {
+  background(50);
+
+  lollipop(200,200,100);
+  lollipop(100,50,70);
+
+}
+
+function lollipop(x,y,diameter){
+  fill(0,200,255);
+  rect(x-10,y,10,130)
+
+  fill(255,0,200);
+  ellipse(x,y,diameter,diameter);
+}
